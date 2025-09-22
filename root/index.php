bacd6608fd0cd9a7c0ba27c98b827173585cb0ca
matsuri-tanii <matsuri.tanii@gmail.com>
2025-09-22 13:23:35 +0900

    fix: enable error reporting in index.php

--- root/index.php
+++ root/index.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
 require_once __DIR__ . '/env.php';
 require_once __DIR__ . '/anon_session.php';
 require_once __DIR__ . '/funcs.php';
